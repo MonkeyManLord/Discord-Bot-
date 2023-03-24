7ab90816df31207b6ef4cc32fa85d7d50066b4a5
Lil Strudel <7326973-Lil-Strudel@users.noreply.gitlab.com>
2023-03-24 17:58:42 -0600

    refac: general cleanup

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import os
 import json
 from dotenv import load_dotenv
 from discord.ext import commands
-load_dotenv(f'-implimented-') 
+load_dotenv()
 
 
 token = os.getenv('token')
@@ -14,58 +14,60 @@ api_key = os.getenv('api_key')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 url = "https://love-calculator.p.rapidapi.com/getPercentage"
 
-#functional
+
+# functional
 @bot.event
 async def on_ready():
-   print(f'Bot logged in as {bot.user}')
+    print(f'Bot logged in as {bot.user}')
 
-#not functional
+
+# not functional
 @bot.command(name='Love')
 async def love_calculator(ctx, *, name2):
-   name1 = ctx.author.name
-   name2 = name2[1:]
-   querystring = {"sname":name2,"fname":name1}
+    name1 = ctx.author.name
+    name2 = name2[1:]
+    querystring = {"sname": name2, "fname": name1}
+
+    # Call the Love Calculator API with the two names and the API key
+    headers = {
+           "X-RapidAPI-Key": api_key,
+           "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
+           }
+
+    print("It shouldve worked")
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # Parse the response and send the result to the user
+    # data = response.json()\
+    data = json.loads(response.text)
+    first = data['fname']
+    second = data['sname']
+    percentage = data['percentage']
+    result = data['result']
+    message = f'Love Percentage between {first} and {second}: {percentage}%'
+
+    await ctx.channel.send(message)
 
 
-   # Call the Love Calculator API with the two names and the API key
-   headers = {
-   "X-RapidAPI-Key": api_key,
-   "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
-   }
-
-   print("It shouldve worked")
-   response = requests.request("GET", url, headers=headers, params=querystring)
-
-
-   # Parse the response and send the result to the user
-   #data = response.json()\
-   data = json.loads(response.text)
-   first = data['fname']
-   second = data['sname']
-   percentage = data['percentage']
-   result = data['result']
-   message = f'Love Percentage between {first} and {second}: {percentage}%'
-   
-   await ctx.channel.send(message)
-
-
-  #not functional
+# not functional
 @bot.event
 async def on_member_join(member):
-   user = member.author
-   await user.send("Hello, World!")
+    user = member.author
+    await user.send("Hello, World!")
 
-#functional
+
+# functional
 @bot.event
 async def on_message(msg):
-   if msg.author != bot.user:
-       if msg.content.lower().startswith('!hi'):
-           await msg.channel.send(f'Hi, {msg.author.display_name}')
+    if msg.author != bot.user:
+        if msg.content.lower().startswith('!hi'):
+            await msg.channel.send(f'Hi, {msg.author.display_name}')
 
-#functional
+
+# functional
 @bot.event
 async def on_message_delete(message):
-   await message.channel.send(f'{message.author.display_name} is acting quite sus :face_with_raised_eyebrow: \n They deleted: {message.content}')
+    await message.channel.send(f'{message.author.display_name} is acting quite sus :face_with_raised_eyebrow: \n They deleted: {message.content}')
+
+
 bot.run(token)
-
-
