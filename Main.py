a3fc2d00a8ad4f7a156faa5ba1f03f60ca89e4b9
Lil Strudel <7326973-Lil-Strudel@users.noreply.gitlab.com>
2023-03-24 19:08:05 -0600

    fix: on_message to process commands + fix love command

--- Main.py
+++ Main.py
@@ -1,18 +1,16 @@
 import discord
 import requests
 import os
-import json
 from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
 
 
-token = os.getenv('token')
-api_key = os.getenv('api_key')
-
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+RAPID_API_URL = "https://love-calculator.p.rapidapi.com/getPercentage"
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-url = "https://love-calculator.p.rapidapi.com/getPercentage"
 
 
 # functional
@@ -21,32 +19,25 @@ async def on_ready():
     print(f'Bot logged in as {bot.user}')
 
 
-# not functional
-@bot.command(name='Love')
-async def love_calculator(ctx, *, name2):
-    name1 = ctx.author.name
-    name2 = name2[1:]
-    querystring = {"sname": name2, "fname": name1}
+# functional
+@bot.command()
+async def love(ctx, name: str):
+    querystring = {"fname": ctx.author.name, "sname": name}
 
-    # Call the Love Calculator API with the two names and the API key
     headers = {
-           "X-RapidAPI-Key": api_key,
-           "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
-           }
+        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
+    }
 
-    print("It shouldve worked")
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    try:
+        response = requests.request("GET", RAPID_API_URL, headers=headers, params=querystring)
+        response.raise_for_status()
 
-    # Parse the response and send the result to the user
-    # data = response.json()\
-    data = json.loads(response.text)
-    first = data['fname']
-    second = data['sname']
-    percentage = data['percentage']
-    result = data['result']
-    message = f'Love Percentage between {first} and {second}: {percentage}%'
+        data = response.json()
 
-    await ctx.channel.send(message)
+        await ctx.channel.send(f'Love Percentage between {data["fname"]} and {data["sname"]}: {data["percentage"]}%')
+    except requests.exceptions.RequestException as errh:
+        await ctx.channel.send(f'Uh Oh... Something broke!\nError: {errh}')
 
 
 # functional
@@ -61,6 +52,7 @@ async def on_message(msg):
     if msg.author != bot.user:
         if msg.content.lower().startswith('!hi'):
             await msg.channel.send(f'Hi, {msg.author.display_name}')
+    await bot.process_commands(msg) # This will run the other commands declared on the bot. Alternatively remove the on_message handler
 
 
 # functional
@@ -69,4 +61,4 @@ async def on_message_delete(message):
     await message.channel.send(f'{message.author.display_name} is acting quite sus :face_with_raised_eyebrow: \n They deleted: {message.content}')
 
 
-bot.run(token)
+bot.run(DISCORD_TOKEN)
