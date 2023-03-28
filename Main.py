b224d3b7db8cf2dbb7dc22e658193e07fc2236d4
Monkey Man <127824308+MonkeyManLord@users.noreply.github.com>
2023-03-27 21:20:44 -0700

    addded cat pic command + joke api

--- Main.py
+++ Main.py
@@ -3,11 +3,13 @@ import requests
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+#from jokeapi import Jokes # Import the Jokes class
+import asyncio
+import json
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
-RAPID_API_URL = "https://love-calculator.p.rapidapi.com/getPercentage"
+
+DISCORD_TOKEN = os.getenv('token')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -17,28 +19,55 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 async def on_ready():
     print(f'Bot logged in as {bot.user}')
 
+# @bot.command(name= "joke")
+# async def startJoke(ctx):
+#     asyncio.run(joke(ctx))
 
+# async def joke(ctx):
+#     j = await Jokes()  # Initialise the class
+#     joke = await j.get_joke()  # Retrieve a random joke
+#     if joke["type"] == "single": # Print the joke
+#         ctx.channel.send(joke["joke"])
+#     else:
+#         ctx.channel.send(joke["setup"])
+#         ctx.channel.send(joke["delivery"])
+    
+
+
+@bot.command(name= "catpic")
+async def fact(ctx):
+    response = requests.get("https://api.thecatapi.com/v1/images/search")
+    data = response.json()
+
+    cat_url = data[0]["url"]
+    await ctx.channel.send(cat_url)
+    
+    
 # functional
-@bot.command()
+@bot.command(name= 'Love')
 async def love(ctx, name: str):
+    LOVE_API_KEY = os.getenv('love_api_key')
+    LOVE_API_URL = "https://love-calculator.p.rapidapi.com/getPercentage"
+    if "@" in name:
+        await ctx.channel.send('Please do not include \'@\' in your love request!')
+        return 
+    name = name.capitalize()
     querystring = {"fname": ctx.author.name, "sname": name}
 
     headers = {
-        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Key": LOVE_API_KEY,
         "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
     }
 
     try:
-        response = requests.request(
-            "GET", RAPID_API_URL, headers=headers, params=querystring)
+        response = requests.request("GET", LOVE_API_URL, headers=headers, params=querystring)
         response.raise_for_status()
 
         data = response.json()
 
-        await ctx.channel.send(f'Love Percentage between {data["fname"]} and {data["sname"]}: {data["percentage"]}%')
+        await ctx.channel.send(f'Love Percentage between {data["fname"]} and {data["sname"]}: {data["percentage"]}%\n{data["result"]}')
     except requests.exceptions.RequestException as errh:
         await ctx.channel.send(f'Uh Oh... Something broke!\nError: {errh}')
-
 
 # functional
 @bot.event
@@ -47,13 +76,12 @@ async def on_member_join(member):
 
 
 # functional
-@bot.event
+@bot.event  
 async def on_message(msg):
     if msg.author != bot.user:
-        if msg.content.lower().startswith('!hi'):
+        if msg.content.lower().startswith('hi'):
             await msg.channel.send(f'Hi, {msg.author.display_name}')
-    # This will run the other commands declared on the bot. Alternatively remove the on_message handler
-    await bot.process_commands(msg)
+    await bot.process_commands(msg) # This will run the other commands declared on the bot. Alternatively remove the on_message handler
 
 
 # functional
@@ -63,3 +91,5 @@ async def on_message_delete(message):
 
 
 bot.run(DISCORD_TOKEN)
+    
+
